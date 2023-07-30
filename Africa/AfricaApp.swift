0a580eabc320244e331b99be164fb2383594d0a0
Calladrus2001 <dugarvishesh@gmail.com>
2023-07-30 12:37:34 +0530

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Vishesh Dugar on 30/07/23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
